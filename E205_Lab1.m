c65801b3e7621849b3b5511f67ac7021a7f3212d
peterjohnsonhmc <peter.johnson.chs@gmail.com>
2020-01-27 09:05:18 -0800

    histogram plotting complete

--- E205_Lab1.m
+++ E205_Lab1.m
@@ -1,4 +1,26 @@
 %% Lab 1 E205
 %pjohnson@g.hmc.edu and pking@g.hmc.edu
 
-%% 1
+%% 1 Read the Data
+N90 = readtable('lab1_azimuth_-90.csv');
+P00 = readtable('lab1_azimuth_00.csv');
+P90 = readtable('lab1_azimuth_90.csv');
+%% 2 Histograms
+figure(1)
+
+histogram(N90.Range_m_);
+title('Histogram of range for azimuth = -90^{\circ}')
+xlabel('Range[m]')
+ylabel('Frequency')
+
+
+histogram(P00.Range_m_);
+title('Histogram of range for azimuth = 0^{\circ}')
+xlabel('Range[m]')
+ylabel('Frequency')
+
+
+histogram(P90.Range_m_);
+title('Histogram of range for azimuth = 90^{\circ}')
+xlabel('Range[m]')
+ylabel('Frequency')
